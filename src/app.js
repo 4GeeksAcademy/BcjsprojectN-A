808ea755ac4152ff7c01dd18498c77be812cf88c
Natalie8a <naty.8a@outlook.com>
2025-04-03 02:37:27 +0000

    jsfinalN8a

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let article = ["A" , "My", "The"];
+  let agent = ["pirate", "grandfather", "president", "cat", "dragon", "robot", "egg"];
+  let action = ["destroyed", "lit up", "crushed", "ate", "evaporated"];
+  let what = ["my homework", "my car", "my phone", "my desk", "my pencil", "my brain","all my food"];
+  let where = ["in the classroom", "in my room", "on top of a cliff", "in Fiji", "right in front of my eyes"]
+  let when = ["before class", "while I was asleep", "during my workout", "five minutes ago", "right before I left"];
+
+
+let rdm1 = Math.floor(Math.random () * article.length);
+let rdm2 = Math.floor(Math.random () * agent.length);
+let rdm3 = Math.floor(Math.random () * action.length);
+let rdm4 = Math.floor(Math.random () * what.length);
+let rdm5 = Math.floor(Math.random () * where.length);
+let rdm6 = Math.floor(Math.random () * when.length);
+
+document.querySelector("#excuse").innerHTML = article [rdm1] + " " + agent [rdm2] + " " + action [rdm3] + " " + what [rdm4] + " " + where [rdm5] + " " + when [rdm6]}
+
